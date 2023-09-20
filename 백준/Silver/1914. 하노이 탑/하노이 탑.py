6cf292798f73acdbfbc85b4ea27dab400a0eced7
EunSeo Ha <121787902+ha-es@users.noreply.github.com>
2023-09-20 14:07:48 +0900

    [Silver I] Title: 하노이 탑, Time: 860 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,15 +1,16 @@
 n = int(input())
 
 
-def hanoi(x, y, z, cnt):
-    if cnt==0:
-        return
+def hanoi(x, y, z, n):
+    if n==1:
+        print(x,z)         
+    else:
+        hanoi(x,z,y,n-1)  
+        print(x,z)
+        hanoi(y,x,z,n-1)  
     
-    hanoi(x, z, y, cnt-1)   # a->b
-    #hanoi(x,y,z,1)         # a->c
-    print(x,z)
-    hanoi(y,x,z, cnt-1)     # b->c
-    
-print(2**n-1)
+print(2**n-1)             
+
+# n이 20이하인 경우
 if(n<=20):
     hanoi(1,2,3,n)
